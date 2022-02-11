73cad9f4d72e1b709484f2db924cd14703ede260
owenlessel <owenlessel@gmail.com>
2022-02-11 09:58:14 -0600

    1-3 Done Start With 4

--- array-list-practice/src/ArrayListPractice.java
+++ array-list-practice/src/ArrayListPractice.java
@@ -11,31 +11,36 @@ public class ArrayListPractice {
         myList.add("Bear");
         myList.add("Dog");
 
-        System.out.println(myList);
+        // System.out.println(myList);
 
         myList.add(2, "Cat");
 
-        System.out.println(myList);
+        // System.out.println(myList);
 
         myList.set(2, "Crab");
 
-        System.out.println(myList);
-
-        myList.remove(1);
+        myList.add("Dog");
 
         System.out.println(myList);
 
-        System.out.println(myList.size());
+        removeDuplicates(myList);
 
-        System.out.println(myList.get(2));
+        // System.out.println(noDupList);
+        // myList.remove(1);
 
-        System.out.println(myList);
+        // System.out.println(myList);
 
-        removeEvenLength(myList);
+        // System.out.println(myList.size());
 
-        System.out.println(myList);
+        // System.out.println(myList.get(2));
 
-        averageVowels(myList);
+        // System.out.println(myList);
+
+        // removeEvenLength(myList);
+
+        // System.out.println(myList);
+
+        // averageVowels(myList);
 
         // int lengthAnimalname = (myList.get(1)).toString().length();
         // System.out.println(lengthAnimalname);
@@ -51,17 +56,22 @@ public class ArrayListPractice {
 
     public static double averageVowels(List<String> myList) {
         double averageNumVowels = 0.0;
-        int vowels=0;
+        int vowels = 0;
         for (String s : myList) {
-            String temp=s.toLowerCase();
+            String temp = s.toLowerCase();
             for (char c : temp.toCharArray()) {
-                    if( c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
-                        vowels=vowels+1;
-                    }
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    vowels = vowels + 1;
                 }
             }
-        
-        averageNumVowels=vowels/myList.size();
+        }
+
+        averageNumVowels = vowels / myList.size();
         return averageNumVowels;
+    }
+
+    public static void removeDuplicates(List<String> myList) {
+        List noDupList = new ArrayList<String>();
+
     }
 }
