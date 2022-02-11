466cf460c77ca67314ff60a96c7e6abef2169df1
owenlessel <owenlessel@gmail.com>
2022-02-11 09:29:20 -0600

    Did Starter Code and 1-3 Working on 4

--- array-list-practice/src/ArrayListPractice.java
+++ array-list-practice/src/ArrayListPractice.java
@@ -1,6 +1,67 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListPractice {
-    public static void main(String[] args) throws Exception {
-        // Add Array List methods from the worksheet here
+    public static void main(String[] args) {
+        List myList = new ArrayList();
+
+        System.out.println(myList);
+
+        myList.add("Ant");
+        myList.add("Bear");
+        myList.add("Dog");
+
+        System.out.println(myList);
+
+        myList.add(2, "Cat");
+
+        System.out.println(myList);
+
+        myList.set(2, "Crab");
+
+        System.out.println(myList);
+
+        myList.remove(1);
+
+        System.out.println(myList);
+
+        System.out.println(myList.size());
+
+        System.out.println(myList.get(2));
+
+        System.out.println(myList);
+
+        removeEvenLength(myList);
+
+        System.out.println(myList);
+
+        averageVowels(myList);
+
+        // int lengthAnimalname = (myList.get(1)).toString().length();
+        // System.out.println(lengthAnimalname);
+    }
+
+    public static void removeEvenLength(List<String> myList) {
+        for (int i = 0; i < myList.size(); i++) {
+            if (myList.get(i).length() % 2 == 0) {
+                myList.remove(i);
+            }
+        }
+    }
+
+    public static double averageVowels(List<String> myList) {
+        double averageNumVowels = 0.0;
+        int vowels=0;
+        for (String s : myList) {
+            String temp=s.toLowerCase();
+            for (char c : temp.toCharArray()) {
+                    if( c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+                        vowels=vowels+1;
+                    }
+                }
+            }
         
+        averageNumVowels=vowels/myList.size();
+        return averageNumVowels;
     }
 }
